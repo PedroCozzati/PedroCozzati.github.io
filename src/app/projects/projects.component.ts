d2dbf40d0a05d1ca601551be7e29fe22ef8f5fc2
PedroCozzati <p.cozzati27@gmail.com>
2024-03-22 16:18:18 -0300

    add new assets

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -22,39 +22,50 @@ export class ProjectsComponent {
       }
     });
   }
-
+    onMouseEnter(event: any): void {
+      event.target.click();
+  }
 
   items =[
     {
-      title:"Java Sprint Boot",
-      image:"https://www.ibm.com/content/dam/adobe-cms/instana/media_logo/Spring.component.complex-narrative-xl.ts=1690565631163.png/content/adobe-cms/br/pt/products/instana/supported-technologies/spring-boot-performance-monitoring/_jcr_content/root/table_of_contents/body/content_section_styled/content-section-body/complex_narrative/logoimage",
-      description:"Crud basico para treinamento",
-      link:'https://github.com/PedroCozzati/Java-Spring-Boot-Alura'
+      title:"Pipeline Airflow - Linkedin",
+      image:"https://www.sicara.fr/hubfs/Copy%20of%20dimension%20site%20%20%2816%29.jpg",
+      description:"",
+      link:'https://github.com/PedroCozzati/pipeline-airflow-etl-linkedin'
     },
     {
-      title:"Manipulação de dados com python",
-      image:"https://spectrum.ieee.org/media-library/the-python-logo-on-top-of-imagery-representing-dna-sequencing-or-other-data.jpg?id=33364099&width=1200&height=900",
-      description:"Projeto da area de dados",
-      link:'https://github.com/PedroCozzati/python-engdados'
+      title:"NY Times Subjects",
+      image:"https://scontent.fcgh67-1.fna.fbcdn.net/v/t39.30808-6/350535703_659199556062511_6399672460381730787_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ZlZ-1OlDiAEAX8wmpPv&_nc_ht=scontent.fcgh67-1.fna&oh=00_AfCsNAsE97AzT42zwiv4i7vfe0FqSRPwSJxVfphjHvXerg&oe=6603886B",
+      description:"",
+      link:'https://github.com/PedroCozzati/ny-times-subject-by-month'
+    },
+    {
+      title:"Streanlit - Análise Linkedin",
+      image:"http://www.ubalt.edu/merrick/uploads/660x450-mobile-homepage/660x450-Infographic%20Icons_Data%20Analytics.jpg",
+      description:"",
+      link:'https://github.com/PedroCozzati/streamlit-analise-linkedin'
     },
     {
       title:"Jogo da trilha",
       image:"https://user-images.githubusercontent.com/80106385/277243596-4d482e91-f19b-4996-bed3-58caadc87c72.png",
-      description:"Feito em angular",
+      description:"",
       link:'https://github.com/PedroCozzati/Jogo_Da_Trilha_PvP_EngIII'
     },
+    {
+      title:"ChatBot",
+      image:"https://private-user-images.githubusercontent.com/80106385/308280670-d21a98cf-74fd-4ea8-a038-eb71342482bb.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTExMzQ4MjgsIm5iZiI6MTcxMTEzNDUyOCwicGF0aCI6Ii84MDEwNjM4NS8zMDgyODA2NzAtZDIxYTk4Y2YtNzRmZC00ZWE4LWEwMzgtZWI3MTM0MjQ4MmJiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzIyVDE5MDg0OFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTU3ZDc3YThjMWViMWY3ZGNlZTg1MmQyNDIwZjJlNzYxZjgwOGI2YzBhZDgwODFjODkyNjA2ZjQ0MDNiYmNhOWMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.A18BO_8uIejZtAo4JgD1rX8vFm9JiX3wP0BthiFOao8",
+      description:"",
+      link:'https://github.com/PedroCozzati/chatbot'
+    },
+    
     {
       title:"App financeiro",
       image:"https://user-images.githubusercontent.com/80106385/224726023-231a5512-f90c-41c4-b51f-b7b876002d68.png",
       description:"Aplicativo de controle financeiro",
       link:'https://github.com/PedroCozzati/financial_control'
-    },
-    {
-      title:"Whatsapp",
-      image:"https://user-images.githubusercontent.com/80106385/277244294-4f51b4ba-767a-42bc-8ae1-180144a3dce0.png",
-      description:"Um dos primeiros projetos",
-      link:'https://github.com/PedroCozzati/python-engdados'
+
     }
+    
 
 
   ]
